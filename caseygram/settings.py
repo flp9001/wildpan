083072c932f01091ca77988231a3b287ac3b75af
Felipe Holanda <felipe.holanda@oowlish.com>
2021-01-13 09:15:15 -0300

    AWS_S3_REGION_NAME

--- caseygram/settings.py
+++ caseygram/settings.py
@@ -153,7 +153,7 @@ DEBUG = os.environ.get("DEBUG_VALUE", False)
 # for s3
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-AWS_S3_REGION_NAME = 'us-east-2'
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
